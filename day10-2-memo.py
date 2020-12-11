28d125145649613e54a8697c7e5ad0b26d9f608a
Martin Moxon <moxon6@googlemail.com>
2020-12-11 12:16:36 +0000

    Update day10-2-memo

--- day10-2-memo.py
+++ day10-2-memo.py
@@ -1,23 +1,16 @@
+from functools import lru_cache
+
 with open("inputs/day10.txt") as f:
     adaptors = list(map(int, f.readlines()))
     adaptors = [ 0, *sorted(adaptors), max(adaptors) + 3]
-    
-    adaptors_set = set(adaptors)
 
-    memo_table = {
-        0: 1
-    }
-
-    def get_sequences_ending_with(x):
-        if x in memo_table:
-            return memo_table[x]
-
-        sequences = 0
-
-        for dx in [1,2,3]:
-            if x - dx in adaptors_set:
-                sequences += get_sequences_ending_with(x - dx)
-        memo_table[x] = sequences
-        return sequences
+    @lru_cache(maxsize=None)
+    def get_sequences_ending_with(x):        
+        if x == 0:
+            return 1
+        return sum(
+            get_sequences_ending_with(x - dx) for dx in [1,2,3] 
+                if x-dx in adaptors
+        )
 
     print(get_sequences_ending_with( max(adaptors) ))
