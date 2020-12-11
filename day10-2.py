584d266e9088d65ef1337d3c7a6f5359303df2b8
Martin Moxon <moxon6@googlemail.com>
2020-12-11 12:03:46 +0000

    Simplify day10-2

--- day10-2.py
+++ day10-2.py
@@ -1,8 +1,6 @@
 with open("inputs/day10.txt") as f:
     adaptors = list(map(int, f.readlines()))
     adaptors = [ 0, *sorted(adaptors), max(adaptors) + 3]
-    
-    adaptors_set = set(adaptors)
 
     sequences_ending_with = {
         0: 1
@@ -13,4 +11,4 @@ with open("inputs/day10.txt") as f:
             if (adaptor - dx) in sequences_ending_with:
                 sequences_ending_with.setdefault(adaptor, 0)
                 sequences_ending_with[adaptor] += sequences_ending_with[adaptor - dx]
-    print(sequences_ending_with)
+    print(sequences_ending_with[max(adaptors)])
