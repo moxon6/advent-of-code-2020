bfe1e59be168c33795c036590938952dc7773f41
Martin Moxon <moxon6@googlemail.com>
2020-12-19 21:10:31 +0000

    day_16_2

--- day_16_2.py
+++ day_16_2.py
@@ -10,61 +10,59 @@ def parse_rule(field_rule):
 def parse_ticket(ticket):
     return [ int(x) for x in ticket.split(",") ]
 
+def get_inputs(string_input):
+    [
+        rules,
+        my_ticket,
+        nearby_tickets_string
+    ] = string_input.split("\n\n")
+    
+    nearby_tickets = map(parse_ticket, nearby_tickets_string.splitlines()[1:])
+    my_ticket = parse_ticket(my_ticket.splitlines()[1])
+
+    validators = {}
+
+    for field in rules.splitlines():
+        field_name, field_rule = field.split(":")
+        validators[field_name] = parse_rule(field_rule)
+    return nearby_tickets, my_ticket, validators
+
+def validate_column(validator, column):
+    return all( validator(val) for val in column )
+
+def is_valid_ticket(validators):
+    def _is_valid_ticket(ticket):
+        return all(
+            any( rule(val) for rule in validators.values() )
+            for val in ticket
+        )
+    return _is_valid_ticket
+
 def main():
     with open("inputs/day16.txt") as f:
-        [
-            rules,
-            my_ticket,
-            nearby_tickets_string
-        ] = f.read().split("\n\n")
-        nearby_tickets = [ parse_ticket(ticket) for ticket in nearby_tickets_string.splitlines()[1:] ]
-
-        my_ticket = parse_ticket(my_ticket.splitlines()[1])
-
-        validators = {}
-
-        for field in rules.splitlines():
-            field_name, field_rule = field.split(":")
-            validators[field_name] = parse_rule(field_rule)
-        
-        ticket_scanning_error_rate = 0
-
-
-        def is_valid_ticket(ticket):
-            return all(
-                any( rule(val) for rule in validators.values() )
-                for val in ticket
-            )
-
-        nearby_tickets = list(filter(is_valid_ticket, nearby_tickets))
-        nearby_tickets = np.array(list(nearby_tickets))
-
-        def validate_column(field_name, validator, column):
-            for i, val in enumerate(column):
-                if not validator(val):
-                    return False
-            return True
-
-        mapping = {}
+        nearby_tickets, my_ticket, validators = get_inputs( f.read() )
+        nearby_valid_tickets = np.array( list(filter(is_valid_ticket(validators), nearby_tickets)) )
+        result_mapping = {}
 
         while len(validators) > 0:
-            for (i, column) in enumerate(nearby_tickets.T):                
-                candidate_fields = []
+            for (i, column) in enumerate(nearby_valid_tickets.T):                
 
-                for (field_name, field_rule) in validators.items():
-                    if validate_column(field_name, field_rule, column):
-                        candidate_fields.append(field_name)
+                candidate_fields = [
+                    field_name for (field_name, field_rule) in validators.items()
+                    if validate_column(field_rule, column)
+                ]
 
                 if len(candidate_fields) == 1:
                     field_name = candidate_fields[0]
                     del validators[field_name]
-                    mapping[field_name] = i
-        
+                    result_mapping[field_name] = i
 
-        total = 1
-        for (field, index) in mapping.items():
-            if field.startswith("departure"):
-                total *= my_ticket[index]
+        total = np.prod([ 
+            my_ticket[index] for (field_name, index) in filter(
+                lambda item: item[0].startswith("departure"),
+                result_mapping.items()
+            )
+        ])
         print(total)
             
 if __name__ == "__main__":
